4e05079d2de635de9ef3fef7090ef7f09d7ec693
IshitaSadaria <ishitasadaria84308@gmail.com>
2025-03-11 00:30:07 +0530

    continuation of functions

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -24,8 +24,52 @@
 
 // ***************************************************************************************************** */
 
-function loginUserMessage(username) {
-    return `${username} just logged in`;
+// function loginUserMessage(username) {
+//     return `${username} just logged in`;
+// }
+
+// console.log(loginUserMessage("Ishita")); //Ishita just logged in
+// if nothing is passed as an argument, it will return undefined just logged in
+
+// ***************************************************************************************************** */
+
+// function loginUserMessage(username) {
+//     if (!username) {
+//         return "Please provide a username";
+//     }
+//     return `${username} just logged in`;
+// }
+
+// console.log(loginUserMessage());
+
+// ***************************************************************************************************** */
+
+// function calculateCartPrice (...num1) {
+//     return num1
+// }
+
+// console.log(calculateCartPrice(100, 200)); //[100, 200]
+
+// ***************************************************************************************************** */
+
+// const user = {
+//     username: 'Ishita',
+//     email: 'ishita@gmail.com'
+// }
+
+// function handleObject (anyObject) {
+//     console.log(`Username is ${anyObject.username} and email is ${anyObject.email}`);
+// }
+
+// handleObject(user); //Username is Ishita and email is ishita@gmail.com
+
+// ***************************************************************************************************** */
+
+const newArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function returnSecondValue (getArray) {
+    return getArray[1];
 }
 
-console.log(loginUserMessage("Ishita")); //Ishita just logged in
+console.log(returnSecondValue(newArray));
+console.log(returnSecondValue([200, 300, 400, 500]));
