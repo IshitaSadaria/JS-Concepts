54a1939d6b2089827d3a71622415030a812f8007
IshitaSadaria <ishitasadaria84308@gmail.com>
2025-03-15 03:03:43 +0530

    project

---
+++ 06_events/async.js
@@ -0,0 +1,10 @@
+// setTimeout( function () {
+//     console.log("Ishita");
+// }, 2000);
+
+//************************************************************************************************* */
+
+const sayHi = function () {
+    console.log("Ishita");
+}
+setTimeout(sayHi, 2000);
